0d33e412ddf3eaeb89513ffa9b28230499e2f368
ajdelfrpymy <alain.delrosario645@gmail.com>
2021-11-08 09:57:03 +0800

    2nd test commit by alain

--- src/com/alain/test/MyTester.java
+++ src/com/alain/test/MyTester.java
@@ -5,4 +5,8 @@ public class MyTester {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
+
+    private void methodOne() {
+        System.out.println("This is method one");
+    }
 }
